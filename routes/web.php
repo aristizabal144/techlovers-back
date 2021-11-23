2dabcfcab23c39ed926828ed6390c9ebb965ef01
andresk-devt <learningxandres@gmail.com>
2021-11-23 17:40:31 -0500

    Se agregan las rutas dentro del middelware

--- routes/web.php
+++ routes/web.php
@@ -19,12 +19,14 @@ $router->post('login', ['as' => 'login', 'uses' => 'UsuariosController@login']);
 $router->post('user', ['as' => 'user.store', 'uses' => 'UsuariosController@store']);
 
 $router->group(['middleware' => 'auth'], function () use ($router) {
+    //AUTH
     $router->get('logout', ['as' => 'logout', 'uses' => 'UsuariosController@logout']);
     $router->get('auth', ['as' => 'auth', 'uses' => 'UsuariosController@showAuth']);
+    
+    //CATEGORIES
+    $router->get('/categorie', 'CategoriasController@index');
+    $router->get('/categorie/{id}', 'CategoriasController@show');
+    $router->post('/categorie', 'CategoriasController@create');
+    $router->put('/categorie/{id}', 'CategoriasController@update');
+    $router->delete('/categorie/{id}', 'CategoriasController@delete');
 });
-
-$router->get('/categorie', 'CategoriasController@index');
-$router->get('/categorie/{id}', 'CategoriasController@show');
-$router->post('/categorie', 'CategoriasController@create');
-$router->put('/categorie/{id}', 'CategoriasController@update');
-$router->delete('/categorie/{id}', 'CategoriasController@delete');
