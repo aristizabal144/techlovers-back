e848c7db3f1394ecd1a49061191d0e3b96af524e
andresk-devt <learningxandres@gmail.com>
2022-10-20 00:05:51 -0500

    Se avanza en los modales y logica del abono

--- app/Http/Controllers/AbonosController.php
+++ app/Http/Controllers/AbonosController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Abonos;
+use App\Models\Factura;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -29,6 +30,9 @@ class AbonosController extends Controller
   public function create(Request $request)
   {
     try {
+
+      DB::beginTransaction();
+
       $abonos = new Abonos;
 
       $abonos->id_factura = $request->id_factura;
@@ -38,7 +42,25 @@ class AbonosController extends Controller
       $abonos->descripcion = $request->descripcion;
 
       $abonos->save();
+
+      $factura = Factura::findOrFail($request->id_factura);
+
+      if ($request->valor_abono > $factura->faltante_pago) {
+        return response()->json([
+          'is_error' => true,
+          'message' => 'No se puede abonar mas de lo que se debe'
+        ]);
+      } else {
+        $factura->faltante_pago = $factura->faltante_pago - $request->valor_abono;
+
+        if ($factura->faltante_pago === 0) {}
+
+        $factura->save();
+      }
+
+      DB::commit();
     } catch (\Throwable $th) {
+      DB::rollback();
       return response()->json([
         'is_error' => $th,
         'message' => 'El registro no se pudo realizar de manera correcta'
